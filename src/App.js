8d1c542e496dfb4d14aa0756503c8c4cec3f39bc
DILHAQUE <95963252+itsdlhqcp@users.noreply.github.com>
2024-10-17 19:01:30 +0530

    made private and public route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import { Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
     <Routes>
-      <Route path='/signin' element={<SignIn />} />
+      <Route
+       path='/signin' 
+       element=
+       {
+        <PublicRoute>
+          <SignIn />
+        </PublicRoute>
+       
+       } />
       <Route 
         path='/' 
         element={
